91f64db4f94319bc2473ba167021af970006c720
John Njenga Ng'ang'a <johnnjeringanga11@gmail.com>
2026-05-19 15:49:05 +0300

    new layout

--- script.js
+++ script.js
@@ -1,62 +1,39 @@
-
-
- 
- 
- document.getElementById("menu").addEventListener("click",()=>{
-  const nav = document.getElementById("mobile-nav")
-  nav.style.display = "flex";
- })
- document.getElementById("close-menu-btn").addEventListener("click",()=>{
-  const nav = document.getElementById("mobile-nav")
-  nav.style.display = "none";
- });
-
-
-  document.getElementById("form").addEventListener("submit", async (e)=>{
+const form = document.getElementById("contactForm");
+const displayer = document.getElementById("displayer");
+  form.addEventListener("submit", async (e)=>{
     e.preventDefault();
 
-
     try {
-      const name = document.getElementById("fullname").value;
-     const email = document.getElementById("email").value;
-      const message = document.getElementById("message").value;
-      if(!name || !email || !message){
-        alert("All fields are required")
+     const name = document.getElementById("nameInput").value;
+     const email = document.getElementById("emailInput").value;
+     const service = document.getElementById("serviceInput").value;
+     const description = document.getElementById("descriptionInput").value;
+      if(!name || !email || !service || !description){
+        displayer.textContent="All fields are required";
       }
       if(!/^[0-9a-zA-Z _.,]+$/.test(name)){
-        alert("name is not good")
+        displayer.textContent="Name contains unwanted symbols";
       }
      else if(!/^[0-9a-zA-Z @_.,]+$/.test(email)){
-        alert("email is not good")
+        displayer.textContent="Email contains unwanted symbols";
       }  
-      else if(!/^[0-9a-zA-Z _.,]+$/.test(message)){
-        alert("message is not good")
-      }                      
+      else if(!/^[0-9a-zA-Z _.,]+$/.test(service)){
+        displayer.textContent="Service contains unwanted symbols";
+      }
+      else if(!/^[0-9a-zA-Z _.,]+$/.test(description)){
+        displayer.textContent="Description contains unwanted symbols";
+      }                           
       else{
+        const message = `Service ${service}
+        Description ${description}`
+
        const parms = {name,email,message};
        const sent = emailjs.send("service_2wfv9zs","template_e5hd66q",parms);
        if(sent){
-   document.getElementById("displayer").textContent = "Your message was sent successfully";
+   displayer.textContent = "Your message was sent successfully";
    } 
       }
     } catch (error) {
-      alert(error)
+      displayer.textContent = error
     }
      });
-
-    
-      
-
- document.getElementById("form").addEventListener("submit", async (e)=>{
-    e.preventDefault();
-
-    const name = document.getElementById("fullname").value;
-     const email = document.getElementById("email").value;
-      const message = document.getElementById("message").value;
-    const parms = {name,email,message};
-
-   const sent = emailjs.send("service_2wfv9zs","template_e5hd66q",parms);
-   if(sent){
-   document.getElementById("displayer").textContent = "Your message was sent successfully";
-   }
- });
